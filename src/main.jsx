f82628d53221d8cc3e500cf8514430982ca9f6fd
joyboy1996 <akundownloadapaja@gmail.com>
2026-08-21 16:27:16 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").catch((e) => console.error("SW register failed", e));
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
